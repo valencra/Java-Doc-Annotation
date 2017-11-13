4a57bf59597c03a898fabc3a5eddae4479516cdc
valencra <valencra@mcmaster.ca>
2017-11-13 18:27:27 -0500

    Initial project files

---
+++ src/com/teamtreehouse/docgen/Doc.java
@@ -0,0 +1,10 @@
+package com.teamtreehouse.docgen;
+
+public @interface Doc {
+
+    /** Description of class or method */
+
+    /** Description of parameters, if annotated element is a method & has parameters */
+
+    /** Description of return value, if annotated element is a method & isn't void */
+}
